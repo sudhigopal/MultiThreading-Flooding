b358a1ca5f9e219f7c24c96acb7ca66262605d28
Sudhindra Gopal krishna <sudhi@ou.edu>
2018-12-19 10:19:50 +0100

    Flooding

---
+++ bashTr.sh
@@ -0,0 +1,63 @@
+
+osascript -e 'tell app "Terminal"
+	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2//Project3.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/1.txt 1"
+end tell' &
+
+osascript -e 'tell app "Terminal"
+	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/Project3.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/2.txt 1"
+end tell' &
+
+osascript -e 'tell app "Terminal"
+	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/Project3.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/3.txt 1"
+end tell' &
+
+osascript -e 'tell app "Terminal"
+	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/Project3.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/TEST2/4.txt 1"
+end tell' 
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/5.txt 1"
+# end tell' &
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/6.txt 1"
+# end tell'
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/7.txt 1"
+# end tell'
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/8.txt 1"
+# end tell'
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/9.txt 1"
+# end tell'
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/10.txt 1"
+# end tell'
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/11.txt 0"
+# end tell'
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/12.txt"
+# end tell'
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/13.txt"
+# end tell'
+# # osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Test/1.0.txt"
+# end tell'
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Test/2.0.txt"
+# end tell'
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Test/3.0.txt"
+# end tell'
+
+# osascript -e 'tell app "Terminal"
+# 	do script "./Desktop/OU_Courses/Data_Networks/Project_3/pcapTest ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Input/Project2Topo.pcap ./Desktop/OU_Courses/Data_Networks/Project_3/Project2Test/4.0.txt"
+# end tell'
